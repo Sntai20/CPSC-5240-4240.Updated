0c6272e01c74bf3a6cf13311d09bcf510ad2ba51
meher sukthi <mehersukthi@gmail.com>
2025-05-06 13:06:03 -0700

    fixes

--- HowTo/interfaces/ITutorialModel.ts
+++ HowTo/interfaces/ITutorialModel.ts
@@ -28,28 +28,5 @@ export interface ITutorialModel extends Mongoose.Document, ITutorial{
 
 }
 
-// interface IStep extends Mongoose.Document {
-//     stepNumber: number;
-//     description: string;
-//     imageUrls: string[];
-//     videoUrls: string[];
-//   }
-  
-//   export interface ITutorialModel extends Mongoose.Document {
-//     title: string;
-//     // description: string;
-//     // tutorialId: string;
-//     // createdDate: Date;
-//     // updatedDate: Date;
-//     // authorId: string;
-//     // authorName: string;
-//     // category: string;
-//     // tags: string[];
-//     // views: number;
-//     // likes: number;
-//     // dislikes: number;
-//     // steps: IStep[];
-//     // published: boolean;
-//   }
   
  
