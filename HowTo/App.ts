522d63595c9e187d32bae008de7aa5768222dd0a
meher sukthi <mehersukthi@gmail.com>
2025-05-06 12:59:54 -0700

    app.ts

--- HowTo/App.ts
+++ HowTo/App.ts
@@ -1,7 +1,5 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import {ListModel} from './model/ListModel';
-import {TaskModel} from './model/TaskModel';
 import { TutorialModel } from './model/TutorialModel';
 import * as crypto from 'crypto';
 
@@ -10,8 +8,6 @@ class App {
 
   // ref to Express instance
   public expressApp: express.Application;
-  public Lists:ListModel;
-  public Tasks:TaskModel;
   public Tutorials:TutorialModel;
 
   //Run configuration methods on the Express instance.
@@ -19,9 +15,6 @@ class App {
   {
     this.expressApp = express();
     this.middleware();
-    this.Lists = new ListModel(mongoDBConnection);
-    this.Tasks = new TaskModel(mongoDBConnection);
-    this.Tasks = new TaskModel(mongoDBConnection);
     this.Tutorials = new TutorialModel(mongoDBConnection);
     this.Tutorials.createModel().then(() => {
       this.routes();  // Only register routes after MongoDB is connected
@@ -44,64 +37,6 @@ class App {
   // Configure API endpoints.
   private routes(): void {
     let router = express.Router();
-    router.get('/app/list/:listId/count', async (req, res) => {
-        var id = req.params.listId;
-        console.log('Query single list with id: ' + id);
-        await this.Tasks.retrieveTasksCount(res, {listId: id});
-    });
-
-    router.get('/app/list/:listId', async (req, res) => {
-      var id = req.params.listId;
-      console.log('Query single list with id: ' + id);
-      await this.Lists.retrieveLists(res, id);
-    });
-
-    router.post('/app/list/', async (req, res) => {
-      const id = crypto.randomBytes(16).toString("hex");
-      console.log(req.body);
-        var jsonObj = req.body;
-        jsonObj.listId = id;
-        try {
-          await this.Lists.model.create([jsonObj]);
-          res.send('{"id":"' + id + '"}');
-        }
-        catch (e) {
-          console.error(e);
-          console.log('object creation failed');
-        }
-    });
-
-    router.post('/app/list2/', async (req, res) => {
-      const id = crypto.randomBytes(16).toString("hex");
-      console.log(req.body);
-        var jsonObj = req.body;
-        jsonObj.listId = id;
-        const doc = new this.Lists.model(jsonObj);
-        try {
-          await doc.save();
-          res.send('{"id":"' + id + '"}');
-        }
-        catch (e) {
-          console.log('object creation failed');
-          console.error(e);
-        }        
-    });
-
-    router.get('/app/list/:listId/tasks', async (req, res) => {
-        var id = req.params.listId;
-        console.log('Query single list with id: ' + id);
-        await this.Tasks.retrieveTasksDetails(res, {listId: id});
-    });
-
-    router.get('/app/list/', async (req, res) => {
-        console.log('Query All list');
-        await this.Lists.retrieveAllLists(res);
-    });
-
-    router.get('/app/listcount', async (req, res) => {
-      console.log('Query the number of list elements in db');
-      await this.Lists.retrieveListCount(res);
-    });
 
     // Get all tutorials
     router.get('/app/tutorials', async (req, res) => {
