33fc8651e11997b69b2d75fdf26960c5022fb5ec
Antonio Santana <ansantan@outlook.com>
2025-05-18 13:31:17 -0700

    Add ESLint disable comment for require imports in App tests

--- test/App.test.ts
+++ test/App.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import * as supertest from 'supertest';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const proxyquire = require('proxyquire').noCallThru();
 
 // Mock TutorialModel and CommentModel
