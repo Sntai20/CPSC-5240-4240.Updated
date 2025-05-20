4cc6486d25089b11ca19d66f437cb700e7824aed
Antonio Santana <ansantan@outlook.com>
2025-05-20 10:27:26 -0700

    Refactor route handlers in commentRoutes, communityNotesRoutes, and tutorialRoutes for improved readability and consistency

--- expressServer/src/routes/commentRoutes.ts
+++ expressServer/src/routes/commentRoutes.ts
@@ -7,9 +7,11 @@ export function commentRoutes(Comments: CommentModel) {
   router.get('/app/comments', async (req, res) => {
     await Comments.retrieveAll(res);
   });
+
   router.get('/app/comments/:id', async (req, res) => {
     await Comments.retrieveByID(res, req.params.id);
   });
+  
   router.post('/app/comments', async (req, res) => {
     await Comments.createComment(res, req.body);
   });
--- expressServer/src/routes/communityNotesRoutes.ts
+++ expressServer/src/routes/communityNotesRoutes.ts
@@ -6,26 +6,22 @@ export function communityNotesRoutes(CommunityNotes: CommunityNoteModel) {
   const router = Router();
 
   router.get('/app/communityNotes', async (req, res) => {
-      await this.CommunityNotes.retrieveAll(res);
-    });
-    router.get('/app/communityNotes/tutorial/:tutorialId', async (req, res) => {
-        await this.CommunityNotes.retrieveByTutorialID(
-          res,
-          req.params.tutorialId
-        );
-      }
-    );
-    router.get(
-      '/app/communityNotes/:noteId',
-      async (req, res) => {
-        await this.CommunityNotes.retrieveByID(res, req.params.noteId);
-      }
-    );
-    router.post('/app/communityNotes', async (req, res) => {
-      const id = req.body.noteId || crypto.randomBytes(8).toString('hex');
-      const jsonObj = { ...req.body, noteId: id };
-      await this.CommunityNotes.createCommunityNote(res, jsonObj);
-    });
+    await CommunityNotes.retrieveAll(res);
+  });
+
+  router.get('/app/communityNotes/tutorial/:tutorialId', async (req, res) => {
+    await CommunityNotes.retrieveByTutorialID(res, req.params.tutorialId);
+  });
+  
+  router.get('/app/communityNotes/:noteId', async (req, res) => {
+    await CommunityNotes.retrieveByID(res, req.params.noteId);
+  });
+  
+  router.post('/app/communityNotes', async (req, res) => {
+    const id = req.body.noteId || crypto.randomBytes(8).toString('hex');
+    const jsonObj = { ...req.body, noteId: id };
+    await CommunityNotes.createCommunityNote(res, jsonObj);
+  });
 
   return router;
 }
