d0c0a1528073636c4e28e3b970dec053b1c938b6
Antonio Santana <ansantan@outlook.com>
2025-05-21 12:05:11 -0700

    Refactor E2E tests to replace tutorial endpoints with comment endpoints

--- expressServer/test/integration/routes/commentRoutes.e2e.test.ts
+++ expressServer/test/integration/routes/commentRoutes.e2e.test.ts
@@ -6,41 +6,32 @@ chai.use(chaiHttp);
 
 describe('CommentModel API endpoints (E2E)', () => {
   var baseUrl = 'http://localhost:8080';
-  describe('POST /app/tutorials (E2E)', () => {
-    it('should create a tutorial and return it', async () => {
-      const newTutorial = {
-        tutorialId: '12345',
-        title: 'Test Tutorial',
-        text: 'This is the main content of the test tutorial.',
-        createdDate: new Date().toISOString(),
-        updatedDate: new Date().toISOString(),
-        authorId: 'author-001',
-        authorName: 'Test Author',
-        category: 'Testing',
-        tags: ['test', 'tutorial'],
-        views: 0,
-        likes: 0,
-        dislikes: 0,
-        steps: [],
-        published: true,
+  describe('POST /app/comments (E2E)', () => {
+    it('should create a comment and return it', async () => {
+      const newComment = {
+        commentId: '12345',
+        noteId: 'note-001',
+        userId: 'user-001',
+        text: 'This is a test comment.',
+        votesUp: 0,
+        votesDown: 0,
       };
       const res = await request.execute(baseUrl)
-        .post('/app/tutorials')
-        .send(newTutorial)
+        .post('/app/comments')
+        .send(newComment)
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json');
       expect(res).to.have.status(201);
       expect(res.body).to.be.an('object');
-      expect(res.body.tutorialId).to.equal(newTutorial.tutorialId);
-      expect(res.body.title).to.equal(newTutorial.title);
-      expect(res.body.published).to.equal(newTutorial.published);
+      expect(res.body.commentId).to.equal(newComment.commentId);
+      expect(res.body.text).to.equal(newComment.text);
     });
   });
 
-  describe('GET /app/tutorials (List Objects) (E2E)', () => {
-    it('should return status 200 for GET /app/tutorials', done => {
+  describe('GET /app/comments (List Objects) (E2E)', () => {
+    it('should return status 200 for GET /app/comments', done => {
       request.execute(baseUrl)
-        .get('/app/tutorials')
+        .get('/app/comments')
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('array').with.length.of.at.least(1);
@@ -49,10 +40,10 @@ describe('CommentModel API endpoints (E2E)', () => {
     });
   });
 
-  describe('GET /app/tutorials (Single Object) (E2E)', () => {
-    it('should return status 200 for GET /app/tutorials/:tutorialId', done => {
+  describe('GET /app/comments (Single Object) (E2E)', () => {
+    it('should return status 200 for GET /app/comments/:commentId', done => {
       request.execute(baseUrl)
-        .get('/app/tutorials/12345')
+        .get('/app/comments/12345')
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('object');
