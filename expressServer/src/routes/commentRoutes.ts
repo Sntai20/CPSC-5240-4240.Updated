4a810327e5d95dcfa8842fd615c4bb27000d9072
Antonio Santana <ansantan@outlook.com>
2025-05-21 12:17:20 -0700

    Add missing line breaks for improved readability in commentRoutes

--- expressServer/src/routes/commentRoutes.ts
+++ expressServer/src/routes/commentRoutes.ts
@@ -7,9 +7,11 @@ export function commentRoutes(Comments: CommentModel) {
   router.get('/app/communityNotes/:noteId/comments', async (req, res) => {
     await Comments.retrieveByNoteID(res, req.params.noteId);
   });
+
   router.get('/app/comments/:commentId', async (req, res) => {
     await Comments.retrieveByID(res, req.params.commentId);
   });
+
   router.post('/app/communityNotes/:noteId/comments', async (req, res) => {
     await Comments.createComment(res, req.body);
   });
