c8b72e2b0bdcfc57749a923af19424cba291d0eb
Antonio Santana <ansantan@outlook.com>
2025-05-21 14:51:01 -0700

    Refactor E2E tests for comments to use a unique ID generator and streamline test cases

--- expressServer/test/integration/routes/commentRoutes.e2e.test.ts
+++ expressServer/test/integration/routes/commentRoutes.e2e.test.ts
@@ -6,84 +6,81 @@ chai.use(chaiHttp);
 
 const baseUrl = 'http://localhost:8080';
 
+interface Comment {
+  commentId: string;
+  noteId: string;
+  userId: string;
+  text: string;
+  votesUp: number;
+  votesDown: number;
+}
+
+function generateUniqueIds() {
+  const uniqueId = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+  return {
+    commentId: uniqueId,
+    noteId: `note-${uniqueId}`,
+    userId: `user-${uniqueId}`,
+  };
+}
+
+async function createComment(comment: Comment) {
+  return request.execute(baseUrl)
+    .post(`/app/communityNotes/${comment.noteId}/comments`)
+    .send(comment)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+}
+
 describe('CommentModel API endpoints (E2E)', () => {
 
-  describe('POST /app/comments (E2E)', () => {
-    const uniqueId = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
-    const uniqueUserId = `user-${uniqueId}`;
-    const uniqueNoteId = `note-${uniqueId}`;
-    it('should create a comment and return it', async () => {
-      const newComment = {
-        commentId: uniqueId,
-        noteId: uniqueNoteId,
-        userId: uniqueUserId,
-        text: 'This is a test comment.',
-        votesUp: 0,
-        votesDown: 0,
-      };
-      const res = await request.execute(baseUrl)
-        .post(`/app/communityNotes/${newComment.noteId}/comments`)
-        .send(newComment)
-        .set('Content-Type', 'application/json')
-        .set('Accept', 'application/json');
-      expect(res).to.have.status(201);
-      expect(res.body).to.be.an('object');
-      expect(res.body.commentId).to.equal(newComment.commentId);
-      expect(res.body.text).to.equal(newComment.text);
-    });
+  it('should create a comment and return it', async () => {
+    const ids = generateUniqueIds();
+    const newComment: Comment = {
+      ...ids,
+      text: 'This is a test comment.',
+      votesUp: 0,
+      votesDown: 0,
+    };
+    const res = await createComment(newComment);
+    expect(res).to.have.status(201);
+    expect(res.body).to.be.an('object');
+    expect(res.body.commentId).to.equal(newComment.commentId);
+    expect(res.body.text).to.equal(newComment.text);
   });
 
-  describe('GET /app/comments by commentId (Single Object) (E2E)', () => {
-    const uniqueId = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
-    const uniqueUserId = `user-${uniqueId}`;
-    const uniqueNoteId = `note-${uniqueId}`;
-    it('should return status 200 for GET /app/comments/:commentId', done => {
-      const newComment = {
-        commentId: uniqueId,
-        noteId: uniqueNoteId,
-        userId: uniqueUserId,
-        text: 'This is a test comment.',
-        votesUp: 0,
-        votesDown: 0,
-      };
+  it('should return status 200 for GET /app/comments/:commentId', async () => {
+    const ids = generateUniqueIds();
+    const newComment: Comment = {
+      ...ids,
+      text: 'This is a test comment.',
+      votesUp: 0,
+      votesDown: 0,
+    };
+    await createComment(newComment);
 
-      request.execute(baseUrl)
-        .get('/app/comments/12345')
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('object');
-          done();
-        });
-    });
+    const res = await request.execute(baseUrl)
+      .get(`/app/comments/${newComment.commentId}`);
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('object');
+    expect(res.body.commentId).to.equal(newComment.commentId);
   });
 
-  describe('GET /app/communityNotes/:noteId/comments by noteId (Single Object) (E2E)', () => {
-      const uniqueId = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
-      const uniqueUserId = `user-${uniqueId}`;
-      const uniqueNoteId = `note-${uniqueId}`;
-      it('should return status 200 for GET /app/communityNotes/:noteId/comments', async () => {
-        const newComment = {
-          commentId: uniqueId,
-          noteId: uniqueNoteId,
-          userId: uniqueUserId,
-          text: 'This is a test comment.',
-          votesUp: 0,
-          votesDown: 0,
-        };
+  it('should return status 200 for GET /app/communityNotes/:noteId/comments', async () => {
+    const ids = generateUniqueIds();
+    const newComment: Comment = {
+      ...ids,
+      text: 'This is a test comment.',
+      votesUp: 0,
+      votesDown: 0,
+    };
+    await createComment(newComment);
 
-        // First, create the comment and wait for it to finish
-        const postRes = await request.execute(baseUrl)
-          .post(`/app/communityNotes/${newComment.noteId}/comments`)
-          .send(newComment)
-          .set('Content-Type', 'application/json')
-          .set('Accept', 'application/json');
-        expect(postRes).to.have.status(201);
+    const res = await request.execute(baseUrl)
+      .get(`/app/communityNotes/${newComment.noteId}/comments`);
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body[0].commentId).to.equal(newComment.commentId);
+  });
 
-        const getRes = await request.execute(baseUrl)
-          .get(`/app/communityNotes/${newComment.noteId}/comments`);
-        expect(getRes).to.have.status(200);
-        expect(getRes.body).to.be.an('array');
-        expect(getRes.body[0].commentId).to.equal(newComment.commentId);
-      });
-    });
 });
